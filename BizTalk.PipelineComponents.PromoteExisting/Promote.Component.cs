31560d6d11a93a1e73f2cf89328e1271f6809fc9
Pires Fernando <Fernando.Malhao.Pires@peab.se>
2018-12-10 19:38:21 +0100

    Init

---
+++ BizTalk.PipelineComponents.PromoteExisting/Promote.Component.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using BizTalkComponents.Utils;
+using System.ComponentModel;
+
+namespace BizTalk.PipelineComponents.PromoteExisting
+{
+    public partial class Promote
+    {
+        public string Name { get { return "PromoteExisting"; } }
+        public string Version { get { return "1.0"; } }
+        public string Description { get { return "Promotes Existing Context"; } }
+
+        [Browsable(false)]
+        public void GetClassID(out Guid classID)
+        {
+            classID = new Guid("bc4ef586-bc79-4be9-b1d7-cda74953a468");
+        }
+        [Browsable(false)]
+        public void InitNew()
+        {
+
+        }
+        [Browsable(false)]
+        public IEnumerator Validate(object projectSystem)
+        {
+            return ValidationHelper.Validate(this, false).ToArray().GetEnumerator();
+        }
+        [Browsable(false)]
+        public bool Validate(out string errorMessage)
+        {
+            var errors = ValidationHelper.Validate(this, true).ToArray();
+
+            if (errors.Any())
+            {
+                errorMessage = string.Join(",", errors);
+
+                return false;
+            }
+
+            errorMessage = string.Empty;
+
+            return true;
+        }
+
+        [Browsable(false)]
+        public IntPtr Icon { get { return IntPtr.Zero; } }
+    }
+}
---
+++ BizTalk.PipelineComponents.PromoteExisting/Promote.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using Microsoft.BizTalk.Streaming;
+using IComponent = Microsoft.BizTalk.Component.Interop.IComponent;
+using BizTalkComponents.Utils;
+
+namespace BizTalk.PipelineComponents.PromoteExisting
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [System.Runtime.InteropServices.Guid("bc4ef586-bc79-4be9-b1d7-cda74953a468")]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    public partial class Promote : IComponent, IBaseComponent, IPersistPropertyBag, IComponentUI
+    {
+
+
+        [DisplayName("Name")]
+        [Description("Context Name")]
+        [RequiredRuntime]
+        public string Property { get; set; }
+
+        [DisplayName("Namespace")]
+        [Description("Context Namespace")]
+        [RequiredRuntime]
+        public string Namespace { get; set; }
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            /*
+           IBaseMessageContext context = pContext.GetMessageFactory().CreateMessageContext();
+
+            for (int i = 0; i < pInMsg.Context.CountProperties; i++)
+            {
+                string name = String.Empty;
+                string ns = String.Empty;
+
+                object value = pInMsg.Context.ReadAt(i, out name, out ns);
+
+                if (pInMsg.Context.IsPromoted(name, ns) || (name == Property && ns == Namespace))
+                {
+                    context.Promote(name, ns, value);
+                }
+                else
+                {
+                    context.Write(name, ns, value);
+                }
+                    
+            }
+
+            pInMsg.Context = context;
+            */
+            object value = pInMsg.Context.Read(Property, Namespace);
+
+            if (value != null && pInMsg.Context.IsPromoted(Property, Namespace) == false)
+                pInMsg.Context.Promote(Property, Namespace, value);
+
+                return pInMsg;
+        }
+
+        //Load and Save are generic, the functions create properties based on the components "public" "read/write" properties.
+        public void Load(IPropertyBag propertyBag, int errorLog)
+        {
+            var props = this.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+
+            foreach (var prop in props)
+
+            {
+
+                if (prop.CanRead & prop.CanWrite)
+
+                {
+
+                    prop.SetValue(this, PropertyBagHelper.ReadPropertyBag(propertyBag, prop.Name, prop.GetValue(this)));
+
+                }
+
+            }
+
+
+        }
+
+        public void Save(IPropertyBag propertyBag, bool clearDirty, bool saveAllProperties)
+        {
+            var props = this.GetType().GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+
+            foreach (var prop in props)
+
+            {
+
+                if (prop.CanRead & prop.CanWrite)
+
+                {
+
+                    PropertyBagHelper.WritePropertyBag(propertyBag, prop.Name, prop.GetValue(this));
+
+                }
+
+            }
+
+        }
+    }
+}
